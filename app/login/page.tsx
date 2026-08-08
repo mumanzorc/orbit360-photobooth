c434ed442bf80a5e10f73192bbe69be0b86b2af7
mumanzorc <mumanzor.c@gmail.com>
2026-08-08 02:05:09 -0400

    agregar app/login/page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,2 @@
+import {redirect} from 'next/navigation';import {currentUser} from '@/lib/auth';import LoginForm from '@/components/LoginForm';
+export default async function Login(){if(await currentUser())redirect('/');return <LoginForm/>}
